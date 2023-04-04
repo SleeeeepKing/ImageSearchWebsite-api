64af89fc2734b6f0e25b1d09d1a7661f648c48e6
Wenlei <luowenlei724@gmail.com>
2023-04-05 01:09:16 +0200

    init

--- src/test/java/com/cytech/imagesearchwebsiteapi/ApplicationTests.java
+++ src/test/java/com/cytech/imagesearchwebsiteapi/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ImageSearchWebsiteApiApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
